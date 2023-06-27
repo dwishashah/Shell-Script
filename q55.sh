72cd2ec5b0c0f8258c3f4d6660dec6bc1d668da3
Dwisha Shah <123867488+dwishashah@users.noreply.github.com>
2023-06-27 09:08:23 +0530

    Biggest of three numbers

---
+++ q55.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter first no:"
+read num1
+echo "Enter second no:"
+read num2
+echo "Enter third no:"
+read num3
+
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
+then 
+ echo '$num1'
+
+elif  [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then 
+ echo $num2
+
+else 
+ echo $num3
+
+fi
